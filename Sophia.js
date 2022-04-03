e05b2775e44691365fea8a8079d2c3b661580045
Sophiaaaaaaaaaaaaaa <1872687690@qq.com>
2022-04-03 13:24:25 -0400

    js2222

---
+++ Sophia.js
@@ -0,0 +1,30 @@
+alert(Date())
+function a1(){
+	document.getElementById('a1').src='https://ok.166.net/reunionpub/1_20200715_1734f0ff422545581.jpeg'
+}
+function a2(){
+	document.getElementById('a2').innerHTML='Oooops';}
+function a3(){
+	alert('Hello')}
+	function a4(){
+	var select= document.getElementById('a4');
+	select.style.backgroundColor='blue';
+	select.style.fontFamily='Arial';
+	select.style.textAlign='center';}
+function sss(f){
+	return(f-32)*5/9;
+}function a5(){
+var select=document.getElementById('a5');
+select.innerHTML=sss(20);}
+
+
+function a6(){
+	var ph=parseFloat(prompt('Number1: '));
+	var hp=parseFloat(prompt('Number2: '));
+	document.getElementById('a6').innerHTML= ph+hp;
+}
+function a7(){
+	var sh = prompt('First name: ');
+	var hs = prompt('Second name: ');
+	document.getElementById('a7').innerHTML= 'Nice to meet you'+ sh + hs;
+}
